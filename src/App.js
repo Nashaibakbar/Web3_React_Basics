c05bf52c7c31b1d05742b06aecfb4a24a5816dd3
Nashiabakbar <nashaibakbar30@gmail.com>
2021-04-02 16:12:28 +0500

    shows chainid and address of connected wallet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+import { useWeb3React } from '@web3-react/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+export const injectedConnector = new InjectedConnector({
+  supportedChainIds:[
+    1, // Mainnet
+    3, // Ropsten
+    4, // Rinkeby
+    5, // Goerli
+    42, // Kovan 
+  ],
+})
+
+const getLibrary = (provider) => {
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000;
+  return library
+  
+}
+
+export const Wallet =() => {
+  const { chainId, account, activate, active } = useWeb3React();
+
+  const onClick = () => {
+    activate(injectedConnector)
+  }
+  return (
+    <div>
+      <div> ChainId: {chainId}</div>
+      <div> Account: {account}</div>
+      {active ? (
+        <dive>✅ </dive>
+      ): (
+        <button type ="button" onClick={onClick}>Connect</button>
+      )}
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Wallet />
+    </Web3ReactProvider>
+  )
 }
 
 export default App;
